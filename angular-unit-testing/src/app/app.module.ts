fc2f18b6bea52ed4fcad8d35b6ff638084935057
vaughan189 <vaughan188@gmail.com>
2020-04-07 15:19:19 +0530

    added angular material

--- angular-unit-testing/src/app/app.module.ts
+++ angular-unit-testing/src/app/app.module.ts
@@ -10,7 +10,13 @@ import { HomeComponent } from './components/home/home.component';
 import { QuoteTextComponent } from './components/quote-text/quote-text.component';
 import { UserComponent } from './components/user/user.component';
 import { UserService } from './components/user/user.service';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,6 +31,13 @@ import { UserService } from './components/user/user.service';
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule
   ],
   providers: [UserService],
   bootstrap: [AppComponent]
